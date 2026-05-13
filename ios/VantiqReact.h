fba1c072fb13d12e192dc96a8a56ef5ea919e6f0
Michael Swan <mswan@vantiq.com>
2026-05-13 14:12:33 -0700

    Updates to ios functionality

--- ios/VantiqReact.h
+++ ios/VantiqReact.h
@@ -1,13 +1,6 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNVantiqReactSpec.h"
-
-@interface VantiqReact : NSObject <NativeVantiqReactSpec>
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
 @interface VantiqReact : RCTEventEmitter <RCTBridgeModule>
-#endif
 
 @end
